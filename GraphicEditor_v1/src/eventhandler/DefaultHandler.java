04d9778ee6d7c9e9b3c93cdf7e486dbb9a7abf78
임기연 <ykl803@naver.com>
2025-03-16 23:51:51 +0900

    디폴트 핸들러수정

--- GraphicEditor_v1/src/eventhandler/DefaultHandler.java
+++ GraphicEditor_v1/src/eventhandler/DefaultHandler.java
@@ -2,24 +2,22 @@ package eventhandler;
 
 import java.awt.event.MouseEvent;
 
-import view.DrawingPanel;
-
 public class DefaultHandler implements MouseEventHandler {
 
 	@Override
-	public void mousePressed(MouseEvent e, DrawingPanel panel) {
+	public void mousePressed(MouseEvent e) {
 		// 아무일도 없어용
 
 	}
 
 	@Override
-	public void mouseReleased(MouseEvent e, DrawingPanel panel) {
+	public void mouseReleased(MouseEvent e) {
 		// 널 객체
 
 	}
 
 	@Override
-	public void mouseDragged(MouseEvent e, DrawingPanel panel) {
+	public void mouseDragged(MouseEvent e) {
 		// 입니다.
 
 	}
